e7f779bdaa337b9d0e04227d1aa229448e26ac4d
Ulrika Höök <ulrika.hook@interactivesolutions.se>
2023-10-12 09:41:14 +0200

    Adding 3d model loading

--- src/three/Scene/Scene.tsx
+++ src/three/Scene/Scene.tsx
@@ -5,6 +5,11 @@ import { CameraControls } from '@react-three/drei'
 import { Floor } from '../Floor';
 import { BackWall } from '../BackWall';
 
+import { useLoader } from '@react-three/fiber'
+
+// @ts-ignore
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+
 interface SceneProps {
   showThingy: boolean;
   floorDimensions: {
@@ -16,6 +21,8 @@ interface SceneProps {
 export const Scene = ({ showThingy, floorDimensions }: SceneProps) => {
   const { width, depth } = floorDimensions;
 
+  const bicycle = useLoader(OBJLoader, '../../../public/swapfiets.obj');
+
   return (
     <div className="canvasWrapper">
       <Canvas shadows camera={{ position: [1.8, 2.2, -4] }}>
@@ -24,6 +31,7 @@ export const Scene = ({ showThingy, floorDimensions }: SceneProps) => {
         <color attach="background" args={['#f0f0f0']} />
         <Floor width={width} depth={depth} />
         <BackWall floorWidth={width} floorDepth={depth} />
+        <primitive object={bicycle} position={[1.5, 0.1, -1.0]}/>
         {showThingy && (
           <mesh position={[0, 0.55, 0]}>
             <meshNormalMaterial />
